c226674a783c864d323e11c5d63065cd942b0b3f
Hawkeye <check@discord.com>
2024-04-04 12:04:59 +0200

    Hardening for setting commandline logfile without directory

--- hawkbot_rt/__init__.py
+++ hawkbot_rt/__init__.py
@@ -1,4 +1,4 @@
-# Pyarmor 8.5.2 (pro), 005619, 2024-04-04T11:18:03.679947
+# Pyarmor 8.5.2 (pro), 005619, 2024-04-04T12:03:41.010179
 from sys import version_info as py_version
 def __pyarmor__():
     import platform
