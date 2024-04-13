508577a9a07ef8bf4c0780104ec12c0c51d50c4b
Hawkeye <check@discord.com>
2024-04-13 22:00:38 +0200

    Bump dependency versions

--- hawkbot_rt/__init__.py
+++ hawkbot_rt/__init__.py
@@ -1,4 +1,4 @@
-# Pyarmor 8.5.2 (pro), 005619, 2024-04-12T15:51:39.250277
+# Pyarmor 8.5.2 (pro), 005619, 2024-04-13T21:58:42.365662
 from sys import version_info as py_version
 def __pyarmor__():
     import platform
